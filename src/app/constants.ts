918d71d90b0603bfdf4ed993895421554b539a29
aarontravass <aaron.travass@hotmail.com>
2023-02-05 09:56:12 -0500

    changed api url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'https://hackviolet2023-backend-yawlfr3euq-uk.a.run.app/';
+// export const API_URL = 'https://hackviolet2023-backend-yawlfr3euq-uk.a.run.app/';
+export const API_URL = 'https://api.trackbites.tech/'
