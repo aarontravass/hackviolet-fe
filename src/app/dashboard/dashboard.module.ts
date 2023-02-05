e39a0823c71e1f92a726bf713fa44b6c51e3a75f
aarontravass <aaron.travass@hotmail.com>
2023-02-05 05:26:19 -0500

    made changes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -8,9 +8,10 @@ import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard.component';
 import { CommonModule } from '@angular/common';
 import { RecipeViewerComponent } from './recipe-viewer/recipe-viewer.component';
+import { LocationSettingsComponent } from './location-settings/location-settings.component';
 
 @NgModule({
     imports: [DashboardRoutingModule, MaterialModule, FormsModule, CommonModule],
-    declarations: [MainComponent, WeightChartComponent, RecipesComponent, DashboardComponent, RecipeViewerComponent]
+    declarations: [MainComponent, WeightChartComponent, RecipesComponent, DashboardComponent, RecipeViewerComponent, LocationSettingsComponent]
 })
 export class DashboardModule {}
